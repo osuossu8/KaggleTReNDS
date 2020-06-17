eeec58b31744070f19ffc71da3f7a8a1329121c3
osuossu8 <osuosuossu18@gmail.com>
2020-06-17 12:32:28 +0000

    exp2

--- src/engine.py
+++ src/engine.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import logging
 import torch
 import torch.nn as nn
 from tqdm import tqdm
@@ -103,8 +104,8 @@ def eval_fn(data_loader, model, device):
     m_all = 0
     for i in range(5):
         m = metric(y_true[:,i], y_pred[:,i])
-        print(domain[i],'metric:', m)
+        logging.info(domain[i],'metric:', m)
         m_all += m*w[i]
 
-    print('all_metric:', m_all)
+    logging.info('all_metric:', m_all)
     return m_all, losses.avg        
